2bc296f2dcca044a71bbfa956bb6cca29b5a0668
hadabas <hadabas12@hotmail.com>
2024-10-25 17:49:50 +0200

    Tesztelés

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир", "Tesztelés"]
 
         self.button = QtWidgets.QPushButton("Click me!")
         self.text = QtWidgets.QLabel("Hello World",
