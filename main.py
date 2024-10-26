6461d6dfd1bfb5573a71b1a325b4a99d54d1a7cf
hadabas <hadabas12@hotmail.com>
2024-10-26 12:17:50 +0200

    Business logic szeperálása a fő gui ablaktól

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sys
-import random
 from PySide6 import QtCore, QtWidgets, QtGui
+from logic import Logic
 
 class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
+        self.logic = Logic()
 
         self.title = 'Kő-Papír-Olló'
         self.setWindowTitle(self.title)
@@ -12,22 +13,15 @@ class MyWidget(QtWidgets.QWidget):
         self.import_button = QtWidgets.QPushButton("Kép importálása")
         self.evaluate_button = QtWidgets.QPushButton("Kép kiértékelése")
         self.text = QtWidgets.QLabel("Várakozás az inputra...", alignment=QtCore.Qt.AlignCenter)
+        
 
         self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.button1)
-        self.layout.addWidget(self.button2)
+        self.layout.addWidget(self.import_button)
+        self.layout.addWidget(self.evaluate_button)
         self.layout.addWidget(self.text)
 
-        self.import_button.clicked.connect(self.import_image)
-        self.evaluate_button.clicked.connect(self.evaluate)
-
-    @QtCore.Slot()
-    def import_image(self):
-        self.text.setText("Kép sikeresen beimportálva")
-
-    @QtCore.Slot()
-    def evaluate(self):
-        self.text.setText(random.choice(["A bal oldali játékos nyert", "A jobb oldali játékos nyert", "Döntetlen"]))
+        self.import_button.clicked.connect(lambda: self.logic.import_image(self.text)) 
+        self.evaluate_button.clicked.connect(lambda: self.logic.evaluate(self.text))
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
