65d28986533d95aa49a766cfdbf46c9c85a5b75c
Peter61045720 <peter61045720@gmail.com>
2024-10-26 10:43:17 +0200

    Add buttons

--- main.py
+++ main.py
@@ -6,21 +6,28 @@ class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир", "Tesztelés"]
+        self.title = 'Kő-Papír-Olló'
+        self.setWindowTitle(self.title)
 
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World",
-                                     alignment=QtCore.Qt.AlignCenter)
+        self.import_button = QtWidgets.QPushButton("Kép importálása")
+        self.evaluate_button = QtWidgets.QPushButton("Kép kiértékelése")
+        self.text = QtWidgets.QLabel("Várakozás az inputra...", alignment=QtCore.Qt.AlignCenter)
 
         self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.button1)
+        self.layout.addWidget(self.button2)
         self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
 
-        self.button.clicked.connect(self.magic)
+        self.button1.clicked.connect(self.import_image)
+        self.button2.clicked.connect(self.evaluate)
 
     @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
+    def import_image(self):
+        self.text.setText("Kép sikeresen beimportálva")
+
+    @QtCore.Slot()
+    def evaluate(self):
+        self.text.setText(random.choice(["A bal oldali játékos nyert", "A jobb oldali játékos nyert", "Döntetlen"]))
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
